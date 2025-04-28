81513fda3c95618779b3beac65fd40ef85616e75
sillas dos santos ribeiro <161138491+sillasSantos00@users.noreply.github.com>
2025-04-28 12:13:43 -0300

    first commit

---
+++ Cadastrodeninjas/src/main/java/dev/java/Cadastrodeninjas/CadastrodeninjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java.Cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeninjasApplication.class, args);
+	}
+
+}
---
+++ Cadastrodeninjas/src/test/java/dev/java/Cadastrodeninjas/CadastrodeninjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.Cadastrodeninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeninjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
