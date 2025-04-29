787fe8288bb725bd417cd0bd5f60fcd572d02e15
sillas dos santos ribeiro <161138491+sillasSantos00@users.noreply.github.com>
2025-04-29 11:31:56 -0300

    Criado a controller

---
+++ Cadastrodeninjas/src/main/java/dev/java/Cadastrodeninjas/Controller.java
@@ -0,0 +1,21 @@
+package dev.java.Cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping("/BOASVINDAS") //pega informacoes QUE ESTA ESCRITO NO METODO BOAS VINDAS AQUI EMBAIXO
+  //  @PostMapping // MANDAR
+    //@PutMapping // ALTERAR
+    //@PatchMapping // ALTERA TBM
+   // @DeleteMapping// DELETA
+
+
+
+    public String boasVindas () {
+        return "essa e minha primeira mensagem nessa rota";
+    }
+}
